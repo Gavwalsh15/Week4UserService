80dcd0ae324a679b943efba028e117a61c962b61
Gavin <G00402296@atu.ie>
2023-10-17 16:03:52 +0100

    Issue with master hope for fix

--- src/main/java/ie/atu/week4userservice/RegistrationServiceClient.java
+++ src/main/java/ie/atu/week4userservice/RegistrationServiceClient.java
@@ -1,4 +1,12 @@
 package ie.atu.week4userservice;
 
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "confirmation-service", url = "http://localhost:8082")
 public interface RegistrationServiceClient {
+    @PostMapping("/confirm")
+    String userDetails(@RequestBody UserDetails userDetails);
 }
